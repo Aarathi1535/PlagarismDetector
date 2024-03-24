41b1b4c43c1a0d7aa6a78164f58d08e40020b064
Balla Aarathisree <121624731+Aarathi1535@users.noreply.github.com>
2024-03-24 22:45:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 from difflib import SequenceMatcher
 import streamlit as st
-st.title("Plagiarism Detector")
-st.markdown("Compare your files to check the percentage of plagiarism present.")
+import random
+st.title("Plagarism Detector")
+st.markdown("Compare your files to check the percentage of plagarism present.")
 f1 = st.file_uploader("Choose file-1",type='txt')
 f2 = st.file_uploader("Choose file-2",type='txt')
-
-if st.button("Check Plagiarism"):
+stored_list= []
+if st.button("Check Plagarism"):
+    random_num = random.randint(1, 1000)
+    if random_num not in stored_list:
+        stored_list.append(random_num)
+    else:
+        random_num = random.randint(1, 1000)
+    id = "Your unique-id is: "+"PD"+str(random_num)
     file1 = f1.getvalue().decode("utf-8")
     file2 = f2.getvalue().decode("utf-8")
     result = SequenceMatcher(None, file1, file2).ratio()
     final = int(result * 100)
-    s = "Plagiarism detected is :"+str(final)+"%"
+    s = "Plagarism detected is :"+str(final)+"%"
     st.text(s)
-
-
+    st.text(id)
