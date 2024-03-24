26a60d2687f44144f480f9c7211242c6c72925d7
Balla Aarathisree <121624731+Aarathi1535@users.noreply.github.com>
2024-03-24 22:10:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from difflib import SequenceMatcher
+import streamlit as st
+st.title("Plagarism Detector")
+st.markdown("Compare your files to check the percentage of plagarism present.")
+f1 = st.file_uploader("Choose file-1",type='txt')
+f2 = st.file_uploader("Choose file-2",type='txt')
+
+if st.button("Check Plagarism"):
+    file1 = f1.getvalue().decode("utf-8")
+    file2 = f2.getvalue().decode("utf-8")
+    result = SequenceMatcher(None, file1, file2).ratio()
+    final = int(result * 100)
+    s = "Plagarism detected is :"+str(final)+"%"
+    st.text(s)
+
+
