3106350d390523e3544006bd27c865433dffa636
Balla Aarathisree <121624731+Aarathi1535@users.noreply.github.com>
2024-03-24 22:26:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.markdown("Compare your files to check the percentage of plagiarism present.")
 f1 = st.file_uploader("Choose file-1",type='txt')
 f2 = st.file_uploader("Choose file-2",type='txt')
 
-if st.button("Check Plagarism"):
+if st.button("Check Plagiarism"):
     file1 = f1.getvalue().decode("utf-8")
     file2 = f2.getvalue().decode("utf-8")
     result = SequenceMatcher(None, file1, file2).ratio()
