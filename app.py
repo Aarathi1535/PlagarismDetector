06c0b38ba4450a377c0079f45edb0ab9d80690de
Balla Aarathisree <121624731+Aarathi1535@users.noreply.github.com>
2024-03-25 10:01:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from difflib import SequenceMatcher
 import streamlit as st
 import random
+import sqlite3
+def get_connection():
+    conn = sqlite3.connect("detections.sqlite")
+    conn.execute("Create Table Detections(Id text, Plagiarism_Percentage text)")
+    return conn
 st.title("Plagiarism Detector")
 st.markdown("Compare your files to check the percentage of plagiarism present.")
 f1 = st.file_uploader("Choose file-1",type='txt')
@@ -12,7 +17,8 @@ if st.button("Check Plagiarism"):
         stored_list.append(random_num)
     else:
         random_num = random.randint(1, 1000)
-    id = "Your unique-id is: "+"PD"+str(random_num)
+    numeric_id = "PD"+str(random_num)
+    id = "Your unique-id is: "+ numeric_id
     file1 = f1.getvalue().decode("utf-8")
     file2 = f2.getvalue().decode("utf-8")
     result = SequenceMatcher(None, file1, file2).ratio()
@@ -20,3 +26,9 @@ if st.button("Check Plagiarism"):
     s = "Plagiarism detected is :"+str(final)+"%"
     st.text(s)
     st.text(id)
+    conn = get_connection()
+    c = conn.cursor()
+    c.execute('INSERT INTO Detections(Id, Plagiarism_Percentage) VALUES (?, ?)', (numeric_id,final))
+    conn.commit()
+
+
